eb8e6b109812857b1dbcbd7c0ed330f95fd670c3
TotoLanson <jbmassee@gmail.com>
2024-07-17 16:45:50 +0200

    Commit initial : microservice diagnosis_notes

---
+++ src/main/java/com/medilabo/diagnosis_notes/DiagnosisNotesApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.diagnosis_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosisNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosisNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medilabo/diagnosis_notes/DiagnosisNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.diagnosis_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosisNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
